414e8a00d817ce3bee67a787f1e56a177795bb6d
Kavi <86911566+ByerDev@users.noreply.github.com>
2025-10-16 15:59:56 +0000

    Reformat Size and Position as tuple structs

--- src/main.rs
+++ src/main.rs
@@ -17,25 +17,23 @@ impl CardinalDirection {
 }
 
 #[derive(Clone, Copy)]
-struct Position {x: usize, y: usize}
+struct Position (usize, usize);
 impl Position {
-  fn new(x: usize, y: usize) -> Self {
-    Position { x: x, y: y }
+  fn new() -> Self {
+    Position(0, 0)
   }
-
   fn as_array(&self) -> [usize; 2] {
-    [self.x, self.y]
+    [self.0, self.1]
   }
 }
 
-struct Size {width: usize, height: usize}
+struct Size (usize, usize);
 impl Size {
-  fn new(width: usize, height: usize) -> Self {
-    Size { width: width, height: height }
+  fn new(size: usize) -> Self {
+    Size(size, size)
   }
-
   fn as_array(&self) -> [usize; 2] {
-    [self.width, self.height]
+    [self.0, self.1]
   }
 }
 
@@ -54,10 +52,10 @@ impl Vector {
     let origin = &self.origin;
     let magnitude = &self.magnitude;
     match self.direction {
-      CardinalDirection::North => Position::new(origin.x, origin.y - magnitude),
-      CardinalDirection::East => Position::new(origin.x + magnitude, origin.y),
-      CardinalDirection::South => Position::new(origin.x, origin.y + magnitude),
-      CardinalDirection::West => Position::new(origin.x - magnitude, origin.y)
+      CardinalDirection::North => Position(origin.0, origin.1 - magnitude),
+      CardinalDirection::East => Position(origin.0 + magnitude, origin.1),
+      CardinalDirection::South => Position(origin.0, origin.1 + magnitude),
+      CardinalDirection::West => Position(origin.0 - magnitude, origin.1)
     }
   }
 }
@@ -72,13 +70,15 @@ impl Rectangle {
   }
 
   fn get_vectors(&self) -> [Vector; 4] {
-    let right = Vector::new(self.origin, CardinalDirection::East, self.size.width);
-    let down = Vector::new(self.origin, CardinalDirection::South, self.size.height);
+    let width = self.size.0;
+    let height = self.size.1
+    let right = Vector::new(self.origin, CardinalDirection::East, self.size.0);
+    let down = Vector::new(self.origin, CardinalDirection::South, self.size.1);
     [
       right,
       down,
-      Vector::new(down.get_end(), CardinalDirection::East, self.size.width),
-      Vector::new(right.get_end(), CardinalDirection::South, self.size.height)
+      Vector::new(down.get_end(), CardinalDirection::East, self.size.0),
+      Vector::new(right.get_end(), CardinalDirection::South, self.size.1)
     ]
   }
 }
@@ -109,13 +109,13 @@ impl Display {
   fn draw_line(&mut self, line: Vector) {
     let axis = line.direction.get_axis();
     if axis == Axis(0) {
-      let mut row = self.pixels.row_mut(line.origin.y);
-      for i in line.origin.x..=line.get_end().x {
+      let mut row = self.pixels.row_mut(line.origin.1);
+      for i in line.origin.0..line.get_end().0 {
         row[i] = true;
       }
     } else {
-      let mut column = self.pixels.column_mut(line.origin.x);
-      for i in line.origin.y..=line.get_end().y {
+      let mut column = self.pixels.column_mut(line.origin.0);
+      for i in line.origin.1..line.get_end().1 {
         column[i] = true;
       }
     }
@@ -131,7 +131,7 @@ impl Display {
 
 
 fn main() {
-  let mut display = Display::new(Size::new(10, 10));
-  display.draw_rect(Rectangle::new(Position::new(1, 1), Size::new(5, 5)));
+  let mut display = Display::new(Size::new(10));
+  display.draw_rect(Rectangle::new(Position::new(), Size::new(10)));
   display.print();
 }
